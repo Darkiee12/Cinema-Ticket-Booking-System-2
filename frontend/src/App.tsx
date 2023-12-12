4303e873169612d12edf6472eca96865fe712b58
Darkness <kizluxury0506@gmail.com>
2023-12-12 22:57:27 +0700

    minor change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
-import FooterComponent from './components/FooterComponent';
-import HeaderComponent from './components/HeaderComponent';
-import ListUserComponent from './components/ListUserComponent';
-import ListMovieComponent from './components/ListMovieComponent';
-import {BrowserRouter as Router} from "react-router-dom";
-import {Routes, Route} from "react-router";
-import AddMovieComponent from './components/AddMovieComponent';
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListUserComponent from "./components/ListUserComponent";
+import ListMovieComponent from "./components/ListMovieComponent";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import AddMovieComponent from "./components/AddMovieComponent";
+import AdminRouter from "./routes/AdminRouter/AdminRouter";
 function App() {
   return (
     <div>
@@ -27,6 +28,8 @@ function App() {
           />
           <Route path="/add-movies" element={<AddMovieComponent />} />
           <Route path="/movies" element={<ListMovieComponent />} />
+          {/* Include AdminRouter here */}
+          <Route path="/admin/*" element={<AdminRouter />} />
         </Routes>
       </Router>
     </div>
